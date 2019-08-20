64084d2f4189f21668bc021475cfcb56c72c23f2
SiGnaLz <e3.141x@gmail.com>
2019-08-19 21:19:31 -0400

    Modified config.js to attach to Heroku DB.

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
     "mysql": {
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'cEr'
+        host: 'us-cdbr-iron-east-02.cleardb.net',
+        user: 'b6922fb80fd9c2',
+        password: '15fe4e6a',
+        database: 'heroku_9b6b18f18895016'
     },
     "port": 3000
 }
