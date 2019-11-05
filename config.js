234a35f59a19a3e174ea78975e12b47ef5bf382f
SiGnaLz <e3.141x@gmail.com>
2019-11-05 15:46:59 -0500

    Updated Config.js File to assist with Local/Heroku deployment

--- config.js
+++ config.js
@@ -2,12 +2,13 @@
 //
 module.exports = {
    "mysql": {
-       host: 'us-cdbr-iron-east-02.cleardb.net',
-       user: 'b6922fb80fd9c2',
-       password: '15fe4e6a',
-       database: 'heroku_9b6b18f18895016'
+       host: 'HEROKU HOST from configVars',
+       user: 'Your UserName',
+       password: 'Your Password',
+       database: 'Your Database'
    }
 }
+
 
 //localhost: uncomment below code
 //
