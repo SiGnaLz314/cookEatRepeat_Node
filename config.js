a68738bae3ac8d9ffaa6e46cfb9843aa3923ea7d
SiGnaLz <e3.141x@gmail.com>
2019-11-27 10:27:37 -0500

    Local Deployment for GitHubPages

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
-//Heroku: uncomment below code
-//
-module.exports = {
-   "mysql": {
-       host: 'HEROKU HOST from configVars',
-       user: 'Your UserName',
-       password: 'Your Password',
-       database: 'Your Database'
-   }
-}
-
-
-//localhost: uncomment below code
-//
+// //Heroku: uncomment below code
+// //
 // module.exports = {
-//     "mysql": {
-//         host: 'localhost',
-//         user: 'root',
-//         password: 'password',
-//         database: 'cEr'
-//     },
-//     "port": 3000
+//    "mysql": {
+//        host: 'HEROKU HOST from configVars',
+//        user: 'Your UserName',
+//        password: 'Your Password',
+//        database: 'Your Database'
+//    }
 // }
+
+
+// localhost: uncomment below code
+
+module.exports = {
+    "mysql": {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'cEr'
+    },
+    "port": 3000
+}
